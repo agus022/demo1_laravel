a4e92e6c634fa32c0d7d5e185ad5c00f8e30ad7e
agus022 <flores.jorge.1j@gmail.com>
2025-02-12 11:31:25 -0600

    index modified

--- resources/views/homeland/index.blade.php
+++ resources/views/homeland/index.blade.php
@@ -87,14 +87,19 @@
             <div class="row mb-5">
                 @foreach ($properties as $propertie)
                 <div class="col-md-6 col-lg-4 mb-4">
-
                         <div class="property-entry h-100">
-                            <a href="property-details.html" class="property-thumbnail">
+                            <a href="#" class="property-thumbnail">
                                 <div class="offer-type-wrap">
                                     <span class="offer-type bg-danger">{{$propertie->offer_type}}</span>
-                                    {{-- <span class="offer-type bg-success">Rent</span> --}}
+                                    <span class="offer-type bg-success">Rent</span>
                                 </div>
-                                <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+                                {{-- recorre todas las imagenes pero unicamente se muestra la primera  --}}
+                                @foreach (json_decode($propertie->$images) as $img)
+                                    @if ($loop->first)
+                                        <img src="{{asset('images')}}/{{$img}}" alt="Image" class="img-fluid">
+                                        @break
+                                    @endif
+                                @endforeach
                             </a>
                             <div class="p-4 property-body">
                                 <a href="#" class="property-favorite"><span class="icon-heart-o"></span></a>
@@ -174,48 +179,6 @@
             </div>
         </div>
     </div>
-
-    <!-- <div class="site-section bg-light">
-        <div class="container">
-          <div class="row justify-content-center mb-5">
-            <div class="col-md-7 text-center">
-              <div class="site-section-title">
-                <h2>Recent Blog</h2>
-              </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis maiores quisquam saepe architecto error corporis aliquam. Cum ipsam a consectetur aut sunt sint animi, pariatur corporis, eaque, deleniti cupiditate officia.</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="100">
-              <a href="#"><img src="images/img_4.jpg" alt="Image" class="img-fluid"></a>
-              <div class="p-4 bg-white">
-                <span class="d-block text-secondary small text-uppercase">Jan 20th, 2019</span>
-                <h2 class="h5 text-black mb-3"><a href="#">Art Gossip by Mike Charles</a></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias enim, ipsa exercitationem veniam quae sunt.</p>
-              </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="200">
-              <a href="#"><img src="images/img_2.jpg" alt="Image" class="img-fluid"></a>
-              <div class="p-4 bg-white">
-                <span class="d-block text-secondary small text-uppercase">Jan 20th, 2019</span>
-                <h2 class="h5 text-black mb-3"><a href="#">Art Gossip by Mike Charles</a></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias enim, ipsa exercitationem veniam quae sunt.</p>
-              </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="300">
-              <a href="#"><img src="images/img_3.jpg" alt="Image" class="img-fluid"></a>
-              <div class="p-4 bg-white">
-                <span class="d-block text-secondary small text-uppercase">Jan 20th, 2019</span>
-                <h2 class="h5 text-black mb-3"><a href="#">Art Gossip by Mike Charles</a></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias enim, ipsa exercitationem veniam quae sunt.</p>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-      </div> -->
-
 
     <div class="site-section bg-light">
         <div class="container">
